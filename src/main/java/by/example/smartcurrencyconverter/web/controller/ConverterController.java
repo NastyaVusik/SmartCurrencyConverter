a02e98f6e44bde3de903d391fdbb9b57b61db692
Anastasiya Vusik <nastyavusik13@gmail.com>
2024-01-10 15:28:51 +0300

    docker-compose.yml was created.

---
+++ src/main/java/by/example/smartcurrencyconverter/web/controller/ConverterController.java
@@ -0,0 +1,16 @@
+package by.example.smartcurrencyconverter.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/converter")
+public class ConverterController {
+
+//    public ResponseEntity<>
+}
